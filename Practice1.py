b0b3f42081ce9a9fc3bbf851b414a6f568b4ccab
Javin27 <119749171+Javin27@users.noreply.github.com>
2025-06-14 12:19:23 +0530

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,8 @@
+a: "str" = input("enter a string !  ")
+def Trial1() -> None:
+    print("its just a trial")
+    print(f"The String You Gave Is \n {a} ")
+
+
+if __name__ == "__main__":
+    Trial1()
